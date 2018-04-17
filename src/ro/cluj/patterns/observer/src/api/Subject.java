4743c9d273e5bbea2610ce55377cbdff043d4c77
Tuddy18 <tuddy18_26@yahoo.com>
2018-04-17 13:56:20 +0300

    subject implementation 1.0

--- src/ro/cluj/patterns/observer/src/api/Subject.java
+++ src/ro/cluj/patterns/observer/src/api/Subject.java
@@ -1,4 +1,55 @@
 package api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subject implements ISubject {
+    List<Observer> observers;
+    String state;
+
+    public Subject() {
+        observers = new ArrayList<>();
+    }
+
+    public Subject(List<Observer> observers) {
+        this.observers = observers;
+    }
+
+    public Subject(List<Observer> observers, String state) {
+        this.observers = observers;
+        this.state = state;
+    }
+
+    public List<Observer> getObservers() {
+        return observers;
+    }
+
+    public void setObservers(List<Observer> observers) {
+        this.observers = observers;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public void addO(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeO(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(Observer obs : observers){
+
+        }
+    }
 }
