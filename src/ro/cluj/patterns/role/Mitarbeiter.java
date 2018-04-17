8c3f1d9a5166accba390ebfcc908d185ba1ddc8d
aliinpop <alinpop97@gmail.com>
2018-04-17 23:29:24 +0300

    new version

---
+++ src/ro/cluj/patterns/role/Mitarbeiter.java
@@ -0,0 +1,14 @@
+package ro.cluj.patterns.role;
+
+public class Mitarbeiter {
+	private String name;
+
+	public Mitarbeiter(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}	
+	
+}
